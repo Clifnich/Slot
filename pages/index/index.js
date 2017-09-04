d65c8c2ea257097e7e3bea38eba4abd2ffb51f40
Puzhen Qian <puqian@ebay.com>
2017-09-04 16:46:56 +0800

    Merge DragNColor to Slot

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,142 +1,121 @@
-//index.js
-//获取应用实例
-var app = getApp()
-Page({
+
+
+Page( {
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    array7: [{ id: '1', color: 'white' }, { id: '2', color: 'white' }, { id: '3', color: 'white' },
-      { id: '4', color: 'white' }, { id: '5', color: 'white' }, { id: '6', color: 'white' }, 
-      { id: '7', color: 'white' }],
-    array: [
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' }, 
-        { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }],
-      [{ color: 'white' }, { color: 'white' }, { color: 'white' },
-      { color: 'white' }, { color: 'white' }, { color: 'white' }, { color: 'white' }]
+    array1: [
+      {id: '1', changeColor: false},
+      {id: '2', changeColor: false},
+      {id: '3', changeColor: false},
+      {id: '4', changeColor: false}
     ],
-    weekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    weekdayInFull: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    marginLeft: 71,
-    marginTop: 61,
-    lastWeekDay: -1,
-    lastHour: -1
+    array2: [
+      {id: '5', changeColor: false},
+      {id: '6', changeColor: false},
+      {id: '7', changeColor: false},
+      {id: '8', changeColor: false}
+    ],
+    array3: [
+      { id: '9', changeColor: false },
+      { id: '10', changeColor: false },
+      { id: '11', changeColor: false },
+      { id: '12', changeColor: false }
+    ],
+    array4: [
+      { id: '13', changeColor: false },
+      { id: '14', changeColor: false },
+      { id: '15', changeColor: false },
+      { id: '16', changeColor: false }
+    ],
+    startWhite: true,
+    windowWidth: 0,
+    windowHeight: 0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  },
-  tapColor: function(e) {
-    console.log(e);
-    var touch = e.touches[0];
-    var x = touch.pageX, y = touch.pageY; 
-    var marginLeft = this.data.marginLeft, marginTop = this.data.marginTop;
-    x -= marginLeft;
-    y -= marginTop;
-    var weekday = (x / 41 + 0.5).toFixed(0);
-    console.log("You just touched block \"" + this.data.weekdayInFull[weekday-1] + "\"");
 
-    var hour = Number((y / 30 + 0.5).toFixed(0)) + 6;
-    var oneMoreHour = Number(hour) + 1;
-    console.log("You just touched block [" + hour + " - " + oneMoreHour + "].");
-
-    var newArray = this.data.array;
-    var element = newArray[hour - 7][weekday - 1];
-    if (element.color === 'green') {
-      element.color = 'white';
-    } else {
-      element.color = 'green';
-    }
-    this.setData({array: newArray});
+  onLoad: function() {
+    var windowWidth, windowHeight;
+    wx.getSystemInfo({
+      success: function (res) {
+        windowWidth = res.windowWidth;
+        windowHeight = res.windowHeight;
+      }
+    });
+    this.setData({
+      windowWidth: windowWidth,
+      windowHeight: windowHeight
+    });
+    console.log("Set windowWidth to be: " + this.data.windowWidth);
+    console.log("Set windowHeight to be: " + this.data.windowHeight);
   },
-  dragToColor: function(e) {
-    console.log(e);
+
+  // this function listens to user's finger movement
+  handletouch: function(e) {
     var touch = e.touches[0];
     var x = touch.pageX, y = touch.pageY;
-    var marginLeft = this.data.marginLeft, marginTop = this.data.marginTop;
-    x -= marginLeft;
-    y -= marginTop;
-    var weekday = (x / 41 + 0.5).toFixed(0);
+    console.log("(" + x + ", " + y + ")");
+    // if x < half of screen width
+    // && y < half of screen height
+    // we are touching block #1, change it to green background
+    var windowWidth = this.data.windowWidth, windowHeight = this.data.windowHeight;
+    var newArray1 = this.data.array1,
+      newArray2 = this.data.array2,
+      newArray3 = this.data.array3,
+      newArray4 = this.data.array4;
+    var halfWidth = windowWidth / 2, quarterHeight = windowHeight / 4;
+    var quarterWidth = halfWidth / 2;
 
-    var hour = Number((y / 30 + 0.5).toFixed(0)) + 6;
-    if (weekday === this.data.lastWeekday && hour === this.data.lastHour)
-      return;
-    var oneMoreHour = Number(hour) + 1;
-    if (this.data.array[hour - 7][weekday - 1].color === 'green')
-      return;
-    var newArray = this.data.array[hour-7];
-    
-    newArray[weekday - 1].color = 'green';
-    var index = hour-7;
-    if (index === 0) {
-      this.setData({ 'array[0]': newArray });
+    var colIndex = this.getColumnIndex(x);
+    console.log('colIndex is: ' + colIndex);
+    var changeColor = this.data.startWhite;
+    if (y < quarterHeight) {
+      console.log("column #" + colIndex + " should change color!");
+      newArray1[colIndex - 1].changeColor = changeColor;
+    } else if (y < quarterHeight * 2) {
+      console.log("column #" + (colIndex + 4) + " should change color!");
+      newArray2[colIndex - 1].changeColor = changeColor;
+    } else if (y < quarterHeight * 3) {
+      console.log("column #" + (colIndex + 8) + " should change color!");
+      newArray3[colIndex - 1].changeColor = changeColor;
+    } else {
+      console.log("column #" + (colIndex + 12) + " should change color!");
+      newArray4[colIndex - 1].changeColor = changeColor;
     }
-    //this.setData({ 'array[{{hour-7}}]': newArray });
+
+    this.setData({
+      array1: newArray1,
+      array2: newArray2,
+      array3: newArray3,
+      array4: newArray4
+    });
   },
-  old_array7DragToColor: function(e) {
-    console.log(e);
+
+  handletouchstart: function(e) {
     var touch = e.touches[0];
-    var x = touch.pageX;
-    x -= this.data.marginLeft;
-    var weekday = (x / 41 + 0.5).toFixed(0);
-    if (this.data.array7[weekday].color !== 'green') {
-      var newArray = this.data.array7;
-      newArray[weekday].color = 'green';
-      this.setData({array7: newArray});
+    var x = touch.pageX, y = touch.pageY;
+
+    var windowWidth = this.data.windowWidth, 
+      windowHeight = this.data.windowHeight;
+
+    var halfWidth = windowWidth / 2, quarterHeight = windowHeight / 4;
+    var quarterWidth = halfWidth / 2;
+
+    var colIndex = this.getColumnIndex(x);
+    console.log('colIndex is: ' + colIndex);
+
+    // change the startWhite attribute
+    var changeColor = this.data.startWhite;
+    if (y < quarterHeight) {
+      this.setData({ startWhite: !this.data.array1[colIndex-1].changeColor});
+    } else if (y < quarterHeight * 2){
+      this.setData({ startWhite: !this.data.array2[colIndex-1].changeColor});
+    } else if (y < quarterHeight * 3){
+      this.setData({ startWhite: !this.data.array3[colIndex - 1].changeColor });
+    } else {
+      this.setData({ startWhite: !this.data.array4[colIndex - 1].changeColor });
     }
   },
-  array7Tap: function(e) {
-    console.log(e);
-    var newArray = this.data.array7;
-    newArray[Number(e.target.id) - 1].color = 'green';
-    this.setData({array7: newArray});
-  },
-  array7DragToColor: function(e) {
-    console.log(e);
-    var newArray = this.data.array7;
-    newArray[Number(e.target.id) - 1].color = 'green';
-    this.setData({ array7: newArray });
+  // This function returns the column index of a given position x
+  // Please -1 to get the actual array index
+  getColumnIndex: function(x) {
+    return (x / (this.data.windowWidth / 4) + 0.5).toFixed(0);
   }
 })
