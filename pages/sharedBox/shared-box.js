5a82864b9916921635286a0a6ae17e1c6c7fb909
Puzhen Qian <puqian@ebay.com>
2017-09-22 17:10:57 +0800

    Going to change sequence of row and column

--- pages/sharedBox/shared-box.js
+++ pages/sharedBox/shared-box.js
@@ -2,30 +2,6 @@
 
 Page( {
   data: {
-    array1: [
-      {id: '1', changeColor: false},
-      {id: '2', changeColor: false},
-      {id: '3', changeColor: false},
-      {id: '4', changeColor: false}
-    ],
-    array2: [
-      {id: '5', changeColor: false},
-      {id: '6', changeColor: false},
-      {id: '7', changeColor: false},
-      {id: '8', changeColor: false}
-    ],
-    array3: [
-      { id: '9', changeColor: false },
-      { id: '10', changeColor: false },
-      { id: '11', changeColor: false },
-      { id: '12', changeColor: false }
-    ],
-    array4: [
-      { id: '13', changeColor: false },
-      { id: '14', changeColor: false },
-      { id: '15', changeColor: false },
-      { id: '16', changeColor: false }
-    ],
     startWhite: true,
     windowWidth: 0,
     windowHeight: 0,
@@ -57,7 +33,9 @@ Page( {
       weekdayArray: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
       numOfRectInRow: 7,
       numOfRectinCol: 17,
-      colorStatus: '010'
+      colorStatus: '010',
+      numOfMembers: 1,
+      hexadecimal: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
   },
 
   onLoad: function(option) {
@@ -107,8 +85,24 @@ Page( {
       numOfRectInRow: count,
       numOfRectInCol: option.endTime - option.startTime + 1,
       weekdayArray: newWeekdayArray,
-      colorStatus: option.colorStatus
+      colorStatus: option.colorStatus,
+      numOfMembers: option.numOfMembers
     });
+
+    // set the colorStatus attribute
+    var numOfBlocks = this.data.numOfRectInCol * this.data.numOfRectInRow;
+    if (option.colorStatus.length < numOfBlocks) {
+      var colorStatusArray = [];
+      for (var i = 0; i < numOfBlocks; i++) {
+        if (i < option.colorStatus.length)
+          colorStatusArray.push(option.colorStatus[i]);
+        else
+          colorStatusArray.push('0');
+      }
+    }
+    this.setData({
+      colorStatus: colorStatusArray.join('')
+    })
   },
 
   // when the elements are ready, draw rectangles on the canvas
@@ -122,15 +116,13 @@ Page( {
     for (var i = 0; i < numOfRectInRow; i++) {
       for (var j = 0; j < numOfRectInColumn; j++) {
         context.setFillStyle('white');
+        context.setStrokeStyle('black');
         context.rect(recWidth * i, recHeight * j, recWidth, recHeight);
         context.stroke();
-        // context.setFillStyle('green');
-        // context.fillRect(recWidth * i, recHeight * j, recWidth, recHeight);
-        if (colorStatus.length > k && colorStatus[k] === '1') {
-          console.log('k is: ' + k);
-          context.setFillStyle('#00ff00');
-          context.fill();
-        }
+        //if (colorStatus.length > k && colorStatus[k] === '1') {
+        context.setFillStyle(this.getRectColor(i * this.data.numOfRectInCol + j));
+        context.fill();
+        //}
         context.draw(true);
         k++;
       }
@@ -142,72 +134,33 @@ Page( {
     });
   },
 
-  // this function listens to user's finger movement
-  // handletouch: function(e) {
-  //   var touch = e.touches[0];
-  //   var x = touch.pageX, y = touch.pageY;
-  //   console.log("(" + x + ", " + y + ")");
-  //   // if x < half of screen width
-  //   // && y < half of screen height
-  //   // we are touching block #1, change it to green background
-  //   var windowWidth = this.data.windowWidth, windowHeight = this.data.windowHeight;
-  //   var newArray1 = this.data.array1,
-  //     newArray2 = this.data.array2,
-  //     newArray3 = this.data.array3,
-  //     newArray4 = this.data.array4;
-  //   var halfWidth = windowWidth / 2, quarterHeight = windowHeight / 4;
-  //   var quarterWidth = halfWidth / 2;
+  getRectColor: function(index) {
 
-  //   var colIndex = this.getColumnIndex(x);
-  //   console.log('colIndex is: ' + colIndex);
-  //   var changeColor = this.data.startWhite;
-  //   if (y < quarterHeight) {
-  //     console.log("column #" + colIndex + " should change color!");
-  //     newArray1[colIndex - 1].changeColor = changeColor;
-  //   } else if (y < quarterHeight * 2) {
-  //     console.log("column #" + (colIndex + 4) + " should change color!");
-  //     newArray2[colIndex - 1].changeColor = changeColor;
-  //   } else if (y < quarterHeight * 3) {
-  //     console.log("column #" + (colIndex + 8) + " should change color!");
-  //     newArray3[colIndex - 1].changeColor = changeColor;
-  //   } else {
-  //     console.log("column #" + (colIndex + 12) + " should change color!");
-  //     newArray4[colIndex - 1].changeColor = changeColor;
-  //   }
+    var colorNumber = 512 / (Number(this.data.numOfMembers) + 1) * Number(this.data.colorStatus[index]);
+    if (colorNumber === 0)
+      return '#ffffff';
+    // console.log('index is: [' + index + '].');
+    // console.log('numOfMembers is: [' + Number(this.data.colorStatus[index]) + '].');
+    console.log('colorNumber is: [' + colorNumber + '].');
+    var result = '#00ff00';
+    if (colorNumber === 256)
+      return result;
+    if (colorNumber < 256) {
+      colorNumber = 256 - colorNumber;
+      var firstBit = this.data.hexadecimal[(colorNumber / 16).toFixed(0)];
+      var secondBit = this.data.hexadecimal[(colorNumber % 16).toFixed(0)];
+      //console.log('first: ' + firstBit + ', second: ' + secondBit);
+      result = '#' + firstBit + secondBit + 'ff' + firstBit + secondBit;
+    } else if (colorNumber > 256) {
+      colorNumber = 256 - (colorNumber - 256);
+      var firstBit = this.data.hexadecimal[(colorNumber / 16).toFixed(0)];
+      var secondBit = this.data.hexadecimal[(colorNumber % 16).toFixed(0)];
+      //console.log('first: ' + firstBit + ', second: ' + secondBit);
+      result = '#00' + firstBit + secondBit + '00';
+    }
 
-  //   this.setData({
-  //     array1: newArray1,
-  //     array2: newArray2,
-  //     array3: newArray3,
-  //     array4: newArray4
-  //   });
-  // },
-
-  // handletouchstart: function(e) {
-  //   var touch = e.touches[0];
-  //   var x = touch.pageX, y = touch.pageY;
-
-  //   var windowWidth = this.data.windowWidth, 
-  //     windowHeight = this.data.windowHeight;
-
-  //   var halfWidth = windowWidth / 2, quarterHeight = windowHeight / 4;
-  //   var quarterWidth = halfWidth / 2;
-
-  //   var colIndex = this.getColumnIndex(x);
-  //   console.log('colIndex is: ' + colIndex);
-
-  //   // change the startWhite attribute
-  //   var changeColor = this.data.startWhite;
-  //   if (y < quarterHeight) {
-  //     this.setData({ startWhite: !this.data.array1[colIndex-1].changeColor});
-  //   } else if (y < quarterHeight * 2){
-  //     this.setData({ startWhite: !this.data.array2[colIndex-1].changeColor});
-  //   } else if (y < quarterHeight * 3){
-  //     this.setData({ startWhite: !this.data.array3[colIndex - 1].changeColor });
-  //   } else {
-  //     this.setData({ startWhite: !this.data.array4[colIndex - 1].changeColor });
-  //   }
-  // },
+    return result;
+  },
 
   dummyStart: function(e) {
     // console.log('start' + e);
