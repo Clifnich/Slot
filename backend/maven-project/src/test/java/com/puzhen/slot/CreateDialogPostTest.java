03b53776d96c863effb504c7cb8eb1d284fb4038
Puzhen Qian <puqian@ebay.com>
2017-09-28 17:12:58 +0800

    Go home.

--- backend/maven-project/src/test/java/com/puzhen/slot/CreateDialogPostTest.java
+++ backend/maven-project/src/test/java/com/puzhen/slot/CreateDialogPostTest.java
@@ -23,14 +23,14 @@ public class CreateDialogPostTest extends TestCase {
 	public void test0() {
 		JsonObject obj = Json.createObjectBuilder()
 				.add("weekdayLine", "0101010")
-				.add("startTime", 8)
-				.add("endTime", 20)
-				.add("numOfMembers", 2)
+				.add("startTime", "8")
+				.add("endTime", "20")
+				.add("numOfMembers", "2")
 				.add("leaderDrawStatus", "010")
 				.add("leader", "puzhen").build();
 		try {
 			HttpURLConnection conn = (HttpURLConnection) 
-					(new URL(urlHead + "/createDialog?leaderId=leader")).openConnection();
+					(new URL(urlHead + "/createDialog")).openConnection();
 			conn.setDoOutput(true);
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("Content-Type", "application/json; charset=utf8");
@@ -43,55 +43,57 @@ public class CreateDialogPostTest extends TestCase {
 		}
 	}
 	
-//	/**
-//	 * This test deliberately misses a parameter in Json,
-//	 * server should return 400 bad request
-//	 */
-//	public void test1(){
-//		JsonObject obj = Json.createObjectBuilder()
-//				.add("weekdayLine", "0101010")
-//				.add("startTime", 8)
-//				.add("numOfMembers", 2)
-//				.add("leaderDrawStatus", "010")
-//				.add("leader", "puzhen").build();
-//		try {
-//			HttpURLConnection conn = (HttpURLConnection)
-//					(new URL(urlHead + "/createDialog?leaderId=leader")).openConnection();
-//			conn.setDoOutput(true);
-//			conn.setRequestMethod("POST");
-//			conn.setRequestProperty("Content-Type", "application/json; charset=utf8");
-//			PrintWriter writer = new PrintWriter(conn.getOutputStream());
-//			writer.write(obj.toString()); writer.flush();
-//			assertEquals(400, conn.getResponseCode());
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//			fail();
-//		}
-//	}
-//	
-//	public void testThatResponseIdIsString() {
-//		JsonObject obj = Json.createObjectBuilder()
-//				.add("weekdayLine", "0101010")
-//				.add("startTime", 8)
-//				.add("endTime", 20)
-//				.add("numOfMembers", 2)
-//				.add("leaderDrawStatus", "010")
-//				.add("leader", "puzhen").build();
-//		try {
-//			HttpURLConnection conn = (HttpURLConnection) 
-//					(new URL(urlHead + "/createDialog?leaderId=leader")).openConnection();
-//			conn.setDoOutput(true);
-//			conn.setRequestMethod("POST");
-//			conn.setRequestProperty("Content-Type", "application/json; charset=utf8");
-//			PrintWriter writer = new PrintWriter(conn.getOutputStream());
-//			writer.write(obj.toString()); writer.flush();
-//			String dialogId = Networks.getResponseFromHttpConnection(conn);
-//			assertTrue(dialogId.length() > 0);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//			fail();
-//		}
-//	}
+	/**
+	 * This test deliberately misses a parameter in Json,
+	 * server should return 400 bad request
+	 */
+	public void test1(){
+		JsonObject obj = Json.createObjectBuilder()
+				.add("weekdayLine", "0101010")
+				.add("startTime", "8")
+				.add("numOfMembers", "2")
+				.add("leaderDrawStatus", "010")
+				.add("leader", "puzhen").build();
+		try {
+			HttpURLConnection conn = (HttpURLConnection)
+					(new URL(urlHead + "/createDialog")).openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json; charset=utf8");
+			PrintWriter writer = new PrintWriter(conn.getOutputStream());
+			writer.write(obj.toString()); writer.flush();
+			assertEquals(400, conn.getResponseCode());
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	public void testThatResponseIdIsString() {
+		JsonObject obj = Json.createObjectBuilder()
+				.add("weekdayLine", "0101010")
+				.add("startTime", "8")
+				.add("endTime", "20")
+				.add("numOfMembers", "2")
+				.add("leaderDrawStatus", "010")
+				.add("leader", "puzhen").build();
+		try {
+			HttpURLConnection conn = (HttpURLConnection) 
+					(new URL(urlHead + "/createDialog")).openConnection();
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json; charset=utf8");
+			PrintWriter writer = new PrintWriter(conn.getOutputStream());
+			writer.write(obj.toString()); writer.flush();
+			String dialogId = Networks.getResponseFromHttpConnection(conn);
+			System.out.println("[" + dialogId + "].");
+			assertTrue(dialogId.length() > 0);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 
 
 	
